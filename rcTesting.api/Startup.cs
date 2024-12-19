72d48fdf1a58c3b4082256854409b9e790856264
ringtail <zhongwei.lzw@alibaba-inc.com>
2024-12-19 15:57:05 +0800

    init branch

---
+++ rcTesting.api/Startup.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace rcTesting.api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc()
+                    .AddJsonOptions(options =>
+                    {
+                        options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                        options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+                        options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                        options.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.None;
+                        options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                    });
+
+            // The following line enables Application Insights telemetry collection.
+            // https://docs.microsoft.com/zh-tw/azure/azure-monitor/app/sampling
+            var aiOptions = new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions();
+            aiOptions.EnableAdaptiveSampling = false;
+            services.AddApplicationInsightsTelemetry(aiOptions);
+            
+            // Adds services required for using options.
+            services.AddOptions();
+
+            services.Configure<GzipCompressionProviderOptions>(options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
+            services.AddResponseCompression(options =>
+            {
+                options.EnableForHttps = true;
+                options.Providers.Add<GzipCompressionProvider>();
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "image/svg+xml" });
+            });
+
+            //Set Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "rcTesting API",
+                    Description = "Ricacorp Service WebApi",
+                    Contact = new OpenApiContact { Name = "Ching Wong", Email = "chingwong@ricacorp.com" },
+                });
+
+                //Set the comments path for the swagger json and ui.
+                //var filePath = Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "rcPostV3.api.xml");
+                var filePath = Path.Combine(Microsoft.DotNet.PlatformAbstractions.ApplicationEnvironment.ApplicationBasePath, "rcTesting.api.xml");
+                c.IncludeXmlComments(filePath);
+            });
+
+            services.AddHttpClient(Options.DefaultName)
+            .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Gzip
+            app.UseResponseCompression();
+
+            // Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "rcTesting");
+            });
+
+            app.UseStaticFiles();
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ rcTesting.api/api.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Fabric.Health;
+using System.IO;
+
+namespace rcTesting.api
+{
+    /// <summary>
+    /// The FabricRuntime creates an instance of this class for each service type instance. 
+    /// </summary>
+    internal sealed class api : StatelessService
+    {
+        public api(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (like tcp, http) for this service instance.
+        /// </summary>
+        /// <returns>The collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+#if DEBUG
+            HealthInformation healthInformation = new HealthInformation("ProjectConfiguration", "Debug", HealthState.Error);
+            this.Partition.ReportInstanceHealth(healthInformation);
+#endif
+
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext =>
+                    new KestrelCommunicationListener(serviceContext, "ServiceEndpoint", (url, listener) =>
+                    {
+                        ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting Kestrel on {url}");
+
+                        return new WebHostBuilder()
+                                    .UseKestrel()
+                                    .ConfigureServices(
+                                        services => services
+                                            .AddSingleton<StatelessServiceContext>(serviceContext))
+                                    .ConfigureAppConfiguration((hostingContext, config) =>
+                                    {
+                                        var env = hostingContext.HostingEnvironment;
+
+                                        config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                                            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                                            .AddEnvironmentVariables()
+                                            .SetBasePath(env.ContentRootPath);
+                                    })
+                                    .ConfigureLogging((hostingContext, logging)  => {
+                                        logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                                        // add ETW logging https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?tabs=aspnetcore2x#eventsource
+                                        // this generate a lot of "The parameters to the Event method do not match the parameters to the WriteEvent method. This may cause the event to be displayed incorrectly." in debug window
+                                        // and no events in Diagnostic Window of Visual Studio, but events itself are recorded (tested with PSH event etl file)
+                                        // see more https://github.com/Microsoft/ApplicationInsights-dotnet-server/issues/608, https://stackoverflow.com/questions/42123222/tpl-etw-events-have-extra-parameters-that-cause-excessive-debugger-output
+                                        logging.AddEventSourceLogger();
+
+                                    })
+                                    .UseContentRoot(Directory.GetCurrentDirectory())
+                                    .UseStartup<Startup>()
+                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
+                                    .UseUrls(url)
+                                    .Build();
+                    }))
+            };
+        }
+    }
+}
